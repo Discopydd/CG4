e9a4fe49a19f71342a5212025573e69ffc6711c6
LE2C_29_リ_ヨン <136088534+Discopydd@users.noreply.github.com>
2025-04-17 12:19:50 +0900

    プロジェクト ファイルを追加します。

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,38 @@
+#include <Windows.h>
+#include"KamataEngine.h"
+#include "sence/GameScene.h"
+
+using namespace KamataEngine;
+
+
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	KamataEngine::Initialize(L"LE3C_28_リ_ヨン");
+	GameScene* gameScene = nullptr;
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	gameScene = new GameScene();
+	gameScene->Initialize();
+	while (true) {
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// ゲームシーンの毎フレーム処理
+		gameScene->Update();
+		// 描画開始
+		dxCommon->PreDraw();
+		// ゲームシーンの描画
+		gameScene->Draw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+	delete gameScene;
+	KamataEngine::Finalize();
+
+	return 0;
+}
