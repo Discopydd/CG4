a8df00f8dc72f392a1471f815c195596756be571
LE2C_29_リ_ヨン <136088534+Discopydd@users.noreply.github.com>
2025-07-03 12:33:08 +0900

    05_02が完了

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include"KamataEngine.h"
 #include "sence/GameScene.h"
+#include "TitleScene.h"
 
 using namespace KamataEngine;
 
@@ -11,29 +12,50 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	KamataEngine::Initialize(L"LE3C_28_リ_ヨン");
 	GameScene* gameScene = nullptr;
+	TitleScene* titleScene = nullptr;
 	// DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
-	gameScene = new GameScene();
-	gameScene->Initialize();
+	titleScene = new TitleScene();
+	titleScene->Initialize();
+
 	while (true) {
 		if (KamataEngine::Update()) {
 			break;
 		}
+		if (titleScene && !titleScene->IsSceneEnd()) {
+			titleScene->Update();
+		}
+		else {
+			if (titleScene) {
+				delete titleScene;
+				titleScene = nullptr;
 
-		// ゲームシーンの毎フレーム処理
-		gameScene->Update();
+				gameScene = new GameScene();
+				gameScene->Initialize();
+			}
+
+			if (gameScene) {
+				gameScene->Update();
+			}
+		}
 		// 描画開始
 		dxCommon->PreDraw();
-		// ゲームシーンの描画
-		gameScene->Draw();
+		if (titleScene && !titleScene->IsSceneEnd()) {
+			titleScene->Draw();
+		}
+		else if (gameScene) {
+			gameScene->Draw();
+		}
 
 		// 描画終了
 		dxCommon->PostDraw();
 	}
-	delete gameScene;
-	gameScene = nullptr;
-	KamataEngine::Finalize();
-
+		delete gameScene;
+		gameScene = nullptr;
+		delete titleScene;
+		titleScene = nullptr;
+		KamataEngine::Finalize();
+	
 	return 0;
 }
